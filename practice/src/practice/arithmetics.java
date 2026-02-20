98582df241a40dfaaff5bb8e2bbda8f09e1a8076
Dhiraj Jadhav <dhirajj2104@gmail.com>
2026-02-20 18:47:00 +0530

    arithmetic operations

---
+++ practice/src/practice/arithmetics.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class arithmetics {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("Arithmetic operations : ");
+		System.out.println("Addition : 10 + 20 : "+(a+b));
+		System.out.println("Substraction : 10 - 20 : "+(a-b));
+		System.out.println("Multiplication : 10 * 20 : "+(a*b));
+		System.out.println("Division : 20 / 10 : "+(b/a));
+	}
+}
